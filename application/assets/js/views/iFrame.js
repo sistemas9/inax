f23c5213b384dfebff3f4010349dde81806fd53a
AYTSistemas13 <sistemas13@avanceytec.com.mx>
2021-07-14 09:09:16 -0600

    Added iFrame to Herramientas de Venta

---
+++ application/assets/js/views/iFrame.js
@@ -0,0 +1,7 @@
+function iFrame(){
+	swal({
+		html: "<iframe src=\"https://ayt-apps.eastus.cloudapp.azure.com/iFrameHerramienta\" title=\"iFrame Herramienta de Ventas\" width=100% height=700px></iframe>",
+		width: '1600px',
+		customClass: 'swal-wide',
+	});
+}
